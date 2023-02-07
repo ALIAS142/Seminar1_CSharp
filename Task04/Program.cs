2918d6b95f323c8e047a7e23897e0821e01fe2c9
Anna <kochurovaav@gmail.com>
2023-02-07 21:48:53 +0200

    Task04 is added

---
+++ Task04/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Enter number: ");
+
+string strNumberN = Console.ReadLine();
+
+int NumberN = Convert.ToInt32(strNumberN);
+
+int NumberK = NumberN * (-1);
+
+do
+{   
+    
+    System.Console.WriteLine(NumberK);
+    NumberK = NumberK + 1;
+} 
+while (NumberK <= 0);
+
+if (NumberK >= 0);
+{   
+    do
+    {
+       System.Console.WriteLine(NumberK);
+       NumberK = NumberK + 1;
+      
+    }
+    while (NumberK <= NumberN);
+
+} 
+
